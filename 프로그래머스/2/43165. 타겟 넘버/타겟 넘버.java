9ef2a84aece11d5c40ac01aa9b2982d31b3dcc21
NamYeonW00 <114655545+NamYeonW00@users.noreply.github.com>
2025-11-19 15:36:44 +0900

    [level 2] Title: 타겟 넘버, Time: 7.14 ms, Memory: 84.8 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,25 +1,6 @@
 class Solution {
-    static int answer;
-    static boolean[] visited;
     public int solution(int[] numbers, int target) {
-        answer = 0;
-        visited = new boolean[numbers.length];
-        dfs(0, target, 0 + numbers[0], numbers);
-        dfs(0, target, 0 - numbers[0], numbers);
-        
+        int answer = 0;
         return answer;
-    }
-    static void dfs(int idx, int target, int sum, int[] numbers) {
-        
-        if (idx == numbers.length - 1) {
-            if (sum == target)
-                answer++;
-            return;
-        }
-        
-        visited[idx] = true;
-        dfs(idx + 1, target, sum + numbers[idx + 1], numbers);
-        dfs(idx + 1, target, sum - numbers[idx + 1], numbers);
-        visited[idx] = false;
     }
 }
