c5840ba75d71df68ab6d27b91f8ed9ac1ffb4aa9
NamYeonW00 <114655545+NamYeonW00@users.noreply.github.com>
2026-01-17 18:00:22 +0900

    [Silver I] Title: 미로 탐색, Time: 112 ms, Memory: 14796 KB -BaekjoonHub

---
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+  static int[] dx = { 0, 1, 0, -1};
+  static int[] dy = { 1, 0, -1, 0};
+  static boolean[][] visited;
+  static int[][] A;
+  static int N, M;
+
+  static void bfs(int i, int j) {
+    Queue<int[]> queue = new LinkedList<>();
+    queue.add(new int[] {i, j});
+    visited[i][j] = true;
+
+    while (!queue.isEmpty()) {
+      int now[] = queue.poll();
+
+      for (int k = 0; k < 4; k++) {
+        int x = now[0] + dx[k];
+        int y = now[1] + dy[k];
+
+        if (x >= 0 && y >= 0 && x < N && y < M) {
+          if (A[x][y] != 0 && !visited[x][y]) {
+            visited[x][y] = true;
+            A[x][y] = A[now[0]][now[1]] + 1;
+            queue.add(new int[] {x, y});
+          }
+        }
+      }
+    }
+  }
+  
+  public static void main(String args[]) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    N = Integer.parseInt(st.nextToken());
+    M = Integer.parseInt(st.nextToken());
+
+    A = new int[N][M];
+    visited = new boolean[N][M];
+
+    for (int i = 0; i < N; i++) {
+      String line = br.readLine();
+      for (int j = 0; j < M; j++) {
+        A[i][j] = line.charAt(j) - '0';
+      }
+    }
+
+    bfs(0, 0);
+    System.out.println(A[N - 1][M - 1]);
+  }
+}
